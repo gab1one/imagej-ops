f0a74ffb35ca56bdd6ee94210f78576864a6e385
Aparna Pal <aparna.pal.1994@gmail.com>
2014-09-18 15:29:44 -0500

    Added five test classes for imagej-ops WIP

---
+++ src/test/java/net/imagej/ops/condition/UnionTest.java
@@ -0,0 +1,21 @@
+package net.imagej.ops.condition;
+ 
+import java.util.ArrayList;
+ 
+import javax.naming.Context;
+ 
+import org.junit.Test;
+ 
+import net.imagej.ops.AbstractOpTest;
+import net.imagej.ops.OpService;
+ 
+public class UnionTest<T> extends AbstractOpTest {
+     
+private ArrayList<Condition<T>> conditions = new ArrayList<Condition<T>>();
+ 
+    @Test
+    public void testUnion()
+    {
+                 
+    }
+}
