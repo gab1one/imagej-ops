4058d19e1594e6c006fc49d596523e59b35e7d4c
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2014-11-24 10:55:02 +0100

    Add Kurtosis of Directionality Feature

--- src/main/java/net/imagej/ops/features/tamura/DefaultTamuraKurtosisOfDirectionality.java
+++ src/main/java/net/imagej/ops/features/tamura/DefaultTamuraKurtosisOfDirectionality.java
@@ -1,12 +1,13 @@
 package net.imagej.ops.features.tamura;
 
-import net.imagej.ops.AbstractOutputFunction;
 import net.imagej.ops.Op;
+import net.imagej.ops.features.tamura.helper.DefaultTamuraKurtosisOfDirectionalityHelper;
 import net.imagej.ops.statistics.tamura.TamuraOps;
-import net.imglib2.IterableInterval;
-import net.imglib2.type.numeric.RealType;
+import org.scijava.ItemIO;
 import org.scijava.Priority;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+
 
 /**
  * Calculates the {@link net.imagej.ops.statistics.tamura.TamuraOps.TamuraKurtosisOfDirectionality} of a {@link net.imglib2.IterableInterval }
@@ -14,24 +15,20 @@ import org.scijava.plugin.Plugin;
  */
 
 @Plugin(type = Op.class, label = TamuraOps.TamuraKurtosisOfDirectionality.LABEL, name = TamuraOps.TamuraKurtosisOfDirectionality.NAME, priority = Priority.VERY_HIGH_PRIORITY)
-public class DefaultTamuraKurtosisOfDirectionality extends
-        AbstractOutputFunction<IterableInterval<? extends RealType<?>>, RealType<?>>
-        implements TamuraFeatures.TamuraGranularityFeature
+public class DefaultTamuraKurtosisOfDirectionality
+        implements TamuraFeatures.TamuraGranularityFeature {
 
-{
-    @Override protected RealType<?> safeCompute(
-            IterableInterval<? extends RealType<?>> input,
-            RealType<?> output) {
-        return null;
-    }
+    @Parameter
+    private DefaultTamuraKurtosisOfDirectionalityHelper directionality;
 
-    @Override public RealType<?> createOutput(
-            IterableInterval<? extends RealType<?>> input) {
-        return null;
-    }
+    @Parameter(type = ItemIO.OUTPUT)
+    private double out;
 
+    @Override public void run() {
+        out = directionality.getFeatureValue();
+        }
+    
     @Override public double getFeatureValue() {
-        return 0;
+        return out;
     }
-
 }
