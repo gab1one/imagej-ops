b2a3feca13f9c23b6f203b8036aec6486b66e49c
Eike Heinz <eike.w.heinz@gmail.com>
2015-08-10 15:14:12 +0200

    add call of normalize(conjugate)

--- src/main/java/net/imagej/ops/filter/phasecorrelation/ImgPhaseCorrelationOp.java
+++ src/main/java/net/imagej/ops/filter/phasecorrelation/ImgPhaseCorrelationOp.java
@@ -5,9 +5,14 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import net.imagej.ops.AbstractFunctionOp;
+import net.imagej.ops.OpService;
 import net.imagej.ops.Ops;
 import net.imagej.ops.Ops.Filter.PhaseCorrelate;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 
 @Plugin(type=Ops.Filter.PhaseCorrelate.class, name=Ops.Filter.PhaseCorrelate.NAME)
@@ -16,17 +21,32 @@ public class ImgPhaseCorrelationOp<T extends RealType<T>> extends AbstractFuncti
 	@Parameter(type=ItemIO.INPUT)
 	private RandomAccessibleInterval<T> interval1;
 	
+	@Parameter(type=ItemIO.INPUT)
+	private float normalizationThreshold;
+	
 	@Parameter(type=ItemIO.INPUT, required=false)
 	private int test;
 	
+	@Parameter
+	private OpService ops;
+	
 	@Override
 	public RandomAccessibleInterval<T> compute(RandomAccessibleInterval<T> input) {
+				
+		// TODO compute FFT, (correlate, multiply => normalizeAndConjugate),
+		// calculate inverse FFT
+		ops.filter().fft(input);
+		ops.filter().fft(interval1);
 		
+		// call normalizeComplexImgOp 
+		ops.image().normalize(input, normalizationThreshold);
+		ops.image().normalizeConjugate((Img<T>) interval1, normalizationThreshold);
+				
 		
-		// TODO Auto-generated method stub
+		// maybe create new output instance instead of using interval1 as output
+		ops.filter().ifft(interval1, input);
 		
-		
-		return null; // return result
+		return interval1; // return result
 	}
 	
 	
