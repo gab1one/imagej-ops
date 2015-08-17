24e21084102695ad957d27589c8ee0faefe2721d
Eike Heinz <eike.w.heinz@gmail.com>
2015-08-17 11:48:43 +0200

    add 'test' for phasecorrelation

---
+++ src/test/java/net/imagej/ops/filter/phaseCorrelate/PhaseCorrelateTest.java
@@ -0,0 +1,28 @@
+package net.imagej.ops.filter.phaseCorrelate;
+
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.img.Img;
+import net.imglib2.realtransform.AffineGet;
+import net.imglib2.type.numeric.real.FloatType;
+
+import org.junit.Test;
+
+
+public class PhaseCorrelateTest extends AbstractOpTest {
+
+	/** 'Test' the phaseCorrelate op */
+	@Test
+	public void phaseCorrelateTest() {
+
+		Img<FloatType> in = generateFloatArrayTestImg(true, new long[] {10,10});
+		Img<FloatType> in2 = generateFloatArrayTestImg(true, new long[] {10,10});
+		AffineGet out1;
+		final float threshold = 1E-5f;
+
+
+		out1 = ops.filter().phaseCorrelate(in, in2, threshold);
+		
+		out1.toString();
+		
+	}
+}
