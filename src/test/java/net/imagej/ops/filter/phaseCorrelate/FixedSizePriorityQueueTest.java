d7e499f015f5240423c9e1cb55dcf37495c5aa8a
Eike Heinz <eike.w.heinz@gmail.com>
2015-08-18 15:16:01 +0200

    add test for fixedSizePriorityQueue

---
+++ src/test/java/net/imagej/ops/filter/phaseCorrelate/FixedSizePriorityQueueTest.java
@@ -0,0 +1,52 @@
+package net.imagej.ops.filter.phaseCorrelate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.imagej.ops.filter.phasecorrelation.FixedSizePriorityQueue;
+
+public class FixedSizePriorityQueueTest {
+
+    private FixedSizePriorityQueue<Integer> m_queue;
+
+    @Before
+    public void setup() {
+        m_queue = new FixedSizePriorityQueue<Integer>(5);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void maxSizeTest() {
+        for (int i = 0; i < 5; i++) {
+            assertTrue("add failed!", m_queue.add(i));
+        }
+
+        assertFalse("add did not fail!", m_queue.add(0));
+        assertEquals("wrong size !", m_queue.size(), 5);
+        assertTrue("add failed", m_queue.add(5));
+        assertEquals("wrong size after adding.", m_queue.size(), 5);
+        assertEquals(m_queue.asList().toArray(new Integer[5]),
+                new Integer[] { 1, 2, 3, 4, 5 });
+
+        assertFalse("add did not fail on duplicate", m_queue.add(5));
+        assertEquals("wrong size!", m_queue.size(), 5);
+        assertEquals(m_queue.asList().toArray(new Integer[5]),
+                new Integer[] { 1, 2, 3, 4, 5 });
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorZeroTest() {
+        m_queue = new FixedSizePriorityQueue<Integer>(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorNegativeTest() {
+        m_queue = new FixedSizePriorityQueue<Integer>(-1);
+    }
+
+}
