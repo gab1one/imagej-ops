883bb091c2adcb16b9584915bb983029b7887a1b
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-08-28 19:41:30 +0200

    fix package name

--- src/test/java/net/imagej/ops/filter/phasecorrelation/FixedSizePriorityQueueTest.java
+++ src/test/java/net/imagej/ops/filter/phasecorrelation/FixedSizePriorityQueueTest.java
@@ -1,4 +1,4 @@
-package net.imagej.ops.filter.phaseCorrelate;
+package net.imagej.ops.filter.phasecorrelation;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
